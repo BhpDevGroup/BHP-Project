e7404b62de9255d1cf3acb5974fea25bbcca435c
bhpdev <bhpdev@hotmail.com>
2018-10-28 13:41:55 +0800

    update

---
+++ sdk/BhpKeyPair/BhpKeyPair.cpp
@@ -0,0 +1,14 @@
+﻿// BhpKeyPair.cpp: 定义应用程序的入口点。
+//
+
+#include "BhpKeyPair.h"
+
+using namespace std;
+
+int main()
+{
+	char value = 0;
+	cout << "Hello CMake。Please press any key then press enter......" << endl;
+	cin >> value;
+	return 0;
+}
