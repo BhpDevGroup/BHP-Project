511582c111c408c368ef0dba163b4d7d387bbcc0
SkyeBlack <296471989@qq.com>
2018-10-31 09:19:58 +0800

    add lockcontract test
    add lockcontract test

---
+++ LockContract/Contract1.cs
@@ -0,0 +1,17 @@
+﻿using Bhp.SmartContract.Framework;
+using Bhp.SmartContract.Framework.Services.Bhp;
+
+namespace LockContract
+{
+    public class Contract1 : SmartContract
+    {
+        public static bool Main(uint timestamp, byte[] pubkey, byte[] signature)
+        {
+            Header header = Blockchain.GetHeader(Blockchain.GetHeight());
+            if (header.Timestamp < timestamp)
+                return false;
+            return VerifySignature(signature, pubkey);
+        }
+    }
+}
+
