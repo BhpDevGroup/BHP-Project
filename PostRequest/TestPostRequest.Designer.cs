0582a3115572b017fa3f05e2190431ccc9537687
Reskorey <1204490370@qq.com>
2018-11-05 17:17:30 +0800

    Test SoftWare For Post Request to RPC

---
+++ PostRequest/TestPostRequest.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PostResquest
+{
+    partial class TestPostRequest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(555, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "end";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(514, 364);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(555, 69);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(611, 364);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "unlock";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(588, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "getbalance";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "single";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 21);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "127.0.0.1:20557";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "请求地址及端口";
+            // 
+            // TestPostRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 484);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "TestPostRequest";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PostRequest/TestPostRequest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace PostResquest
+{
+    public partial class TestPostRequest : Form
+    {
+        private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer bTimer;
+        public TestPostRequest()
+        {
+            InitializeComponent();
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            aTimer = new System.Timers.Timer();
+
+            //注册计时器的事件
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            //设置时间间隔为2秒（2000毫秒），覆盖构造函数设置的间隔
+            aTimer.Interval = 120000;
+            //设置是执行一次（false）还是一直执行(true)，默认为true
+            aTimer.AutoReset = true;
+
+            bTimer = new System.Timers.Timer();
+            //注册计时器的事件
+            bTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent2);
+            //设置时间间隔为2秒（2000毫秒），覆盖构造函数设置的间隔
+            bTimer.Interval = 10000;
+            //设置是执行一次（false）还是一直执行(true)，默认为true
+            bTimer.AutoReset = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        string sdsasdasd(string json)
+        {
+            try
+            {
+                string postAddress = textBox1.Text.Trim();
+                HttpClient client = new HttpClient();
+                //string uri = "127.0.0.1:20557";
+                string uri = postAddress;
+                client.BaseAddress = new Uri($"http:\\\\{postAddress}");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                StringContent theContent = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = client.PostAsync(uri, theContent).Result;
+                Task<System.IO.Stream> task = response.Content.ReadAsStreamAsync();
+                System.IO.Stream backStream = task.Result;
+                System.IO.StreamReader reader = new System.IO.StreamReader(backStream);
+                string json1 = reader.ReadToEnd();
+                return json1;
+            }
+            catch (Exception e)
+            {
+                return "error";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            aTimer.Start();
+            bTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+            bTimer.Stop();
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            //unlock请求
+            string json = " {  \"jsonrpc\": \"2.0\",  \"method\": \"unlock\",  \"params\": [\"1\",1],  \"id\": 1} ";
+            
+            this.Invoke(new Action(() => { listBox1.Items.Add(sdsasdasd(json)); })); 
+        }
+        private void OnTimedEvent2(object source, ElapsedEventArgs e)
+        {
+            //getblance
+            string json = " { \"jsonrpc\": \"2.0\", \"method\": \"getbalance\",\"params\": [\"0x13f76fabfe19f3ec7fd54d63179a156bafc44afc53a7f07a7a15f6724c0aa854\"],\"id\": 1}";
+            this.Invoke(new Action(() => { listBox2.Items.Add(sdsasdasd(json)); }));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //unlock请求
+            string json = " {  \"jsonrpc\": \"2.0\",  \"method\": \"unlock\",  \"params\": [\"1\",1],  \"id\": 1} ";
+            this.Invoke(new Action(() => { listBox1.Items.Add(sdsasdasd(json)); }));
+
+            //getblance
+            string json2 = " { \"jsonrpc\": \"2.0\", \"method\": \"getbalance\",\"params\": [\"0x13f76fabfe19f3ec7fd54d63179a156bafc44afc53a7f07a7a15f6724c0aa854\"],\"id\": 1}";
+            this.Invoke(new Action(() => { listBox2.Items.Add(sdsasdasd(json2)); }));
+
+        }
+    }
+}
